abe188850c07af92f4184edefd6127d7ec64a20f
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-27 18:36:56 -0500

    initial commit

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const userRouter = require('../users/router');
+
+const server = express();
+
+// middleware
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+// routes
+server.use('/api/users', userRouter);
+
+server.get('/', (req, res) => {
+    res.json({ api: 'up' })
+});
+
+module.exports = server;
